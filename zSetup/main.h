46ab8cb4b6a3336e825d704c1e7eb9c9f9462129
Raunaq Gupta <raunaq.rg@gmail.com>
2013-09-28 17:59:15 -0400

    Added main.h and put loop in zSetup

---
+++ zSetup/main.h
@@ -0,0 +1,38 @@
+// record rate - must be in the range 4000 to 44100 samples per second
+// best to use standard values like 8000, 11025, 16000, 22050, 44100
+#define RECORD_RATE 22050
+//#define RECORD_RATE 44100
+//
+// max recorded file size.  Size should be a multiple of cluster size.
+// the recorder creates and erases a contiguous file of this size.
+// 100*1024*1024 bytes - about 100 MB or 150 minutes at 11025 samples/second
+#define MAX_FILE_SIZE 104857600UL  // 100 MB
+//#define MAX_FILE_SIZE 1048576000UL // 1 GB
+//
+// Analog pin connected to mic preamp
+#define MIC_ANALOG_PIN 0
+//
+// Voltage Reference Selections for ADC
+//#define ADC_REFERENCE ADC_REF_AREF  // use voltage on AREF pin
+ #define ADC_REFERENCE ADC_REF_AVCC  // use 5V VCC
+//
+// print the ADC range while recording if > 0
+// print adcMax,adcMin if > 1
+#define DISPLAY_RECORD_LEVEL 1
+//
+// print file info - useful for debug
+#define PRINT_FILE_INFO 0
+//
+// print bad wave file size and SD busy errors for debug
+#define PRINT_DEBUG_INFO 1
+//------------------------------------------------------------------------------
+// global variables
+Sd2Card card;           // SD/SDHC card with support for version 2.00 features
+SdVolume vol;           // FAT16 or FAT32 volume
+SdFile root;            // volume's root directory
+SdFile file;            // current file
+WaveRP wave;            // wave file recorder/player
+int16_t lastTrack = -1; // Highest track number
+uint8_t trackList[32];  // bit list of used tracks
+unsigned long deleteWhilePlayingTimer;  //tracks whether we should delete when playing BMCHANGE
+int potentialDeleteWhilePlaying; // holds combination of currently playing file BMCHANGE
--- zSetup/zSetup.ino
+++ zSetup/zSetup.ino
@@ -1,4 +1,10 @@
 #include <math.h> // for calculating the current combination
+#include <SdFat.h>
+#include <WaveRP.h>
+#include <SdFatUtil.h>
+#include <ctype.h>
+#include "main.h"
+
 int currentComb; //stores the combination of buttons that are currently pressed
 int recordingLED = 7; 
 
@@ -7,4 +13,46 @@ void setup(){
   waveSetup(); //remainder of original setup functions from the library
   checkButtonsSetup(); //setup from rewritten debounce code
   pinMode(recordingLED,OUTPUT); //allows the led to glow at full brightness/
+}
+
+//Main loop file of the ball of secrets
+//It incorporates some of the code from the other files (WaveRecordPlay) which need to be looped through
+//The main chunk of the loop is the central area where its essentially debounce code but for currentComb,
+//which represents the sum of the pressed buttons.
+
+unsigned long ccDebounceTimer; //currentComb debounce timer
+int previousComb; //currentComb debounce comparison
+
+void loop(){  
+  //FROM WAVERECORDPLAY
+  if (file.isOpen()) file.close();
+  //scan root dir to build track list and set lastTrack
+  scanRoot();
+  while (Serial.read() >= 0) {}
+  //PgmPrintln("\ntype a command or h for help");
+  int16_t track = -1;
+  uint8_t c;
+
+
+  while(track < 256){
+    //start by checking the switches to get the currentCoimb
+    currentComb = check_switches(); //get the current value of the switches, prints currentComb
+
+    if(currentComb > 0){ //Only proceed if some button is pressed
+
+      if ((ccDebounceTimer + 1000) < millis()) { //is it time to debounce?
+        //Serial.println("passed time check, now entering debounce check");
+        if(currentComb == previousComb){ //check that the buttons pressed are the same
+          searchForFile(currentComb); //proceed with playing or recording
+        }
+        ccDebounceTimer = millis(); //set the timer
+        previousComb = currentComb; //set previous to current.
+      } 
+    }
+   
+    //while (!Serial.available()) {} //loops in here s/serial input; keeping for debugging
+    c = Serial.read();
+    if (!isdigit(c)); break;
+    track = (track < 0 ? 0 : 10 * track) + c - '0';
+  } 
 }
